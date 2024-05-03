5cc20b34f0e630bc224df8b7a9c5a64e0ab4d08f
Dominik Schmid <schmid.dominik.muc@gmail.com>
2024-05-03 12:57:41 +0200

    adding kernel and miden-lib

--- rust_lib/src/lib.rs
+++ rust_lib/src/lib.rs
@@ -1,26 +1,31 @@
 use miden_stdlib::StdLibrary;
+use miden_lib::MidenLib;
 use miden_vm::Assembler;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn compile() -> Result<(), JsValue> {
-    let program =
-        "use.std::math::u64
+    let program = "use.std::math::u64
 
         begin
             push.1.0
             push.2.0
             exec.u64::overflowing_add
         end";
-    
+
+    let kernel = include_str!("api.masm");
+
     let assembler = Assembler::default()
+        .with_library(&MidenLib::default())
+        .expect("failed to load miden-lib")
         .with_library(&StdLibrary::default())
-        .map_err(|err| format!("Failed to load stdlib - {}", err))?
-        .with_debug_mode(true);
+        .expect("failed to load std-lib")
+        .with_kernel(kernel)
+        .expect("kernel must be well formed");
 
     assembler
         .compile(program)
         .map_err(|err| format!("Failed to assemble program - {}", err))?;
-    
+
     Ok(())
 }
--- rust_lib/target/wasm32-unknown-unknown/debug/deps/rust_lib.d
+++ rust_lib/target/wasm32-unknown-unknown/debug/deps/rust_lib.d
@@ -1,5 +1,6 @@
-/Users/dschmid/min-example/rust_lib/target/wasm32-unknown-unknown/debug/deps/rust_lib.wasm: src/lib.rs
+/Users/dschmid/min-example/rust_lib/target/wasm32-unknown-unknown/debug/deps/rust_lib.wasm: src/lib.rs src/api.masm
 
-/Users/dschmid/min-example/rust_lib/target/wasm32-unknown-unknown/debug/deps/rust_lib.d: src/lib.rs
+/Users/dschmid/min-example/rust_lib/target/wasm32-unknown-unknown/debug/deps/rust_lib.d: src/lib.rs src/api.masm
 
 src/lib.rs:
+src/api.masm:
